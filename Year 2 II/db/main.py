3cc6ae8ed3f00b097bc2daee5f1cb30ce1a46374
VoidTest <92273689+VoidTest@users.noreply.github.com>
2024-10-30 15:09:55 +0200

    a

--- Year 2 II/db/main.py
+++ Year 2 II/db/main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
-
+import sqlite3
+from flask import Flask, render_template, request
+from dati import pievienot_skolenu
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
+    if request.method == "POST":
+        vards = request.form['name']
+        uzvards = request.form['lastname']
+        dati = vards+uzvards
+        pievienot_skolenu(vards, uzvards)
+        return render_template("index.html", aizsutītais = dati)
     return render_template("index.html")
+
+@app.route("/pievienot")
+def pievienot():
+    return render_template("pievienot.html")
+
+@app.route("/atzimes")
+def atzimes():
+    return render_template("atzimes.html")
+
 
 
 if __name__ == '__main__':
