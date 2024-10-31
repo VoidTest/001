c89a5775d03ecb61105d62337c8c77cc893606bf
VoidTest <92273689+VoidTest@users.noreply.github.com>
2024-10-31 16:39:50 +0200

    a

--- Year 2 II/db/dati.py
+++ Year 2 II/db/dati.py
@@ -16,8 +16,36 @@ def tabulas_izveide():
     )
     conn.commit()
 
+def skolotaju_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        CREATE TABLE skolotaji(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        vards TEXT NOT NULL,
+        uzvards TEXT NOT NULL
+        )
+        """
+    )
+    conn.commit()
+
+def prieksmetu_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        CREATE TABLE prieksmeti(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        prieksmets TEXT NOT NULL
+        )
+        """
+    )
+    conn.commit()
+
+#prieksmetu_tabulas_izveide()
 #tabulas_izveide()
+#skolotaju_tabulas_izveide()
 def pievienot_skolenu(vards, uzvards):
+    print(vards, uzvards)
     cur = conn.cursor()
     cur.execute(
         f"""
@@ -25,3 +53,57 @@ def pievienot_skolenu(vards, uzvards):
         """
     )
     conn.commit()
+
+
+def pievienot_skolotaju(vards, uzvards):
+    print(vards, uzvards)
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        INSERT INTO skolotaji(vards, uzvards) VALUES("{vards}","{uzvards}")
+        """
+    )
+    conn.commit()
+
+def pievienot_prieksmetu(prieksmets):
+    print(prieksmets)
+    cur = conn.cursor()
+    cur.execute(
+        f"""
+        INSERT INTO prieksmeti(prieksmets) VALUES("{prieksmets}")
+        """
+    )
+    conn.commit()
+
+def iegut_skolenus():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT vards, uzvards FROM skoleni
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_skolotajus():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT vards, uzvards, id FROM skolotaji
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_prieksmetus():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT prieksmets FROM prieksmeti
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
