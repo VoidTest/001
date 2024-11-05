254e8464572965c291e608907d1f34b9e9d71d54
VoidTest <92273689+VoidTest@users.noreply.github.com>
2024-11-05 12:39:47 +0200

    a

--- Year 2 II/db/dati.py
+++ Year 2 II/db/dati.py
@@ -3,7 +3,7 @@ import sqlite3
 
 conn = sqlite3.connect("dati.db", check_same_thread=False)
 
-def tabulas_izveide():
+def skolenu_tabulas_izveide():
     cur = conn.cursor()
     cur.execute(
         """
@@ -15,6 +15,7 @@ def tabulas_izveide():
         """
     )
     conn.commit()
+
 
 def skolotaju_tabulas_izveide():
     cur = conn.cursor()
@@ -35,15 +36,50 @@ def prieksmetu_tabulas_izveide():
         """
         CREATE TABLE prieksmeti(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        prieksmets TEXT NOT NULL
+        nosaukums TEXT NOT NULL
         )
         """
     )
     conn.commit()
 
-#prieksmetu_tabulas_izveide()
-#tabulas_izveide()
+def atzimju_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        CREATE TABLE atzimes(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        atzime INTEGER NOT NULL,
+        skolena_id INTEGER NOT NULL,
+        prieksmeta_id INTEGER NOT NULL,
+    	FOREIGN KEY (skolena_id) REFERENCES skoleni(id),
+        FOREIGN KEY (prieksmeta_id) REFERENCES prieksmeti(id)
+        )
+        """
+    )
+    conn.commit()
+
+def sk_prieksmetu_tabulas_izveide():
+    cur = conn.cursor()
+    cur.execute(
+        """
+        CREATE TABLE sk_prieksmeti(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        skolotaja_id INTEGER NOT NULL,
+        prieksmeta_id INTEGER NOT NULL,
+    	FOREIGN KEY (skolotaja_id) REFERENCES skolotaji(id),
+        FOREIGN KEY (prieksmeta_id) REFERENCES prieksmeti(id)
+        )
+        """
+    )
+    conn.commit()
+#sk_prieksmetu_tabulas_izveide()
 #skolotaju_tabulas_izveide()
+
+#skolenu_tabulas_izveide()
+
+#prieksmetu_tabulas_izveide()
+
+#atzimju_tabulas_izveide()
 def pievienot_skolenu(vards, uzvards):
     print(vards, uzvards)
     cur = conn.cursor()
@@ -56,42 +92,61 @@ def pievienot_skolenu(vards, uzvards):
 
 
 def pievienot_skolotaju(vards, uzvards):
-    print(vards, uzvards)
     cur = conn.cursor()
     cur.execute(
-        f"""
-        INSERT INTO skolotaji(vards, uzvards) VALUES("{vards}","{uzvards}")
-        """
+    f"""
+    INSERT INTO skolotaji(vards, uzvards) VALUES("{vards}","{uzvards}")
+    """
+    )
+
+    print(vards, uzvards)
+
+def pievienot_prieksmetu(prieksmets):
+    cur = conn.cursor()
+    cur.execute(
+    f"""
+    INSERT INTO prieksmeti(nosaukums) VALUES("{prieksmets}")
+    """
     )
     conn.commit()
 
-def pievienot_prieksmetu(prieksmets):
-    print(prieksmets)
+def pievienot_atzimi(atzime, skolens, prieksmets):
     cur = conn.cursor()
     cur.execute(
-        f"""
-        INSERT INTO prieksmeti(prieksmets) VALUES("{prieksmets}")
-        """
+    f"""
+    INSERT INTO atzimes(atzime, skolena_id, prieksmeta_id) VALUES("{atzime}","{skolens}","{prieksmets}")
+    """
     )
+    print("eouhqwufhwe")
     conn.commit()
+
+def pievienot_sk_prieksmetu(skolotajs, prieksmets):
+    cur = conn.cursor()
+    cur.execute(
+    f"""
+    INSERT INTO sk_prieksmeti(skolotaja_id, prieksmeta_id) VALUES("{skolotajs}","{prieksmets}")
+    """
+    )
+    print("eouhqwufhwe")
+    conn.commit()
+
+
+
 
 def iegut_skolenus():
     cur = conn.cursor()
     cur.execute(
-        """
-        SELECT vards, uzvards FROM skoleni
-        """
+        """SELECT vards, uzvards, id FROM skoleni"""
     )
     conn.commit()
     dati = cur.fetchall()
     return dati
 
+
 def iegut_skolotajus():
     cur = conn.cursor()
     cur.execute(
-        """
-        SELECT vards, uzvards, id FROM skolotaji
-        """
+        """SELECT vards, uzvards, id FROM skolotaji"""
     )
     conn.commit()
     dati = cur.fetchall()
@@ -100,10 +155,35 @@ def iegut_skolotajus():
 def iegut_prieksmetus():
     cur = conn.cursor()
     cur.execute(
-        """
-        SELECT prieksmets FROM prieksmeti
+        """SELECT nosaukums, id FROM prieksmeti"""
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+def iegut_atzimes():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT vards, uzvards, nosaukums, atzime FROM
+        (atzimes JOIN skoleni ON skoleni.id = atzimes.skolena_id)
+        JOIN prieksmeti ON prieksmeti.id = atzimes.prieksmeta_id
         """
     )
     conn.commit()
     dati = cur.fetchall()
     return dati
+
+def iegut_sk_prieksmetus():
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT vards, uzvards, nosaukums FROM
+        (sk_prieksmeti JOIN skolotaji ON skolotaji.id = sk_prieksmeti.skolotaja_id)
+        JOIN prieksmeti ON prieksmeti.id = sk_prieksmeti.prieksmeta_id
+        """
+    )
+    conn.commit()
+    dati = cur.fetchall()
+    return dati
+
+
+
--- Year 2 II/db/main.py
+++ Year 2 II/db/main.py
@@ -1,18 +1,17 @@
 import sqlite3
-from flask import Flask, render_template, request
-from dati import pievienot_skolenu, pievienot_skolotaju, pievienot_prieksmetu, iegut_skolenus, iegut_skolotajus, iegut_prieksmetus
+from flask import Flask, render_template, request, redirect
+from dati import iegut_skolotajus, pievienot_skolenu, pievienot_prieksmetu, pievienot_skolotaju, iegut_skolenus, iegut_prieksmetus, pievienot_atzimi, iegut_atzimes, iegut_sk_prieksmetus
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST","GET"])
 def index():
     skoleni_no_db = iegut_skolenus()
-    print(skoleni_no_db)
+    # print(skoleni_no_db)
     skolotaji_no_db = iegut_skolotajus()
-    print(skolotaji_no_db)
     prieksmeti_no_db = iegut_prieksmetus()
-    print(prieksmeti_no_db)
-
+        
     if request.method == "POST":
         vards = request.form['name']
         uzvards = request.form['lastname']
@@ -25,22 +24,32 @@ def index():
             pievienot_skolotaju(skolotajs_v, skolotajs_uzv)
         if prieksmets:
             pievienot_prieksmetu(prieksmets)
-        
-        dati = f"Pievienots skolēns - {vards} {uzvards} Pievienots skolotajs - {skolotajs_v} {skolotajs_uzv} Pievienots prieksmets - {prieksmets}"
-        return render_template("index.html", aizsutītais = dati, skoleni = skoleni_no_db, skolotaji = skolotaji_no_db, prieksmeti = prieksmeti_no_db)
-    return render_template("index.html", skoleni = skoleni_no_db, skolotaji = skolotaji_no_db, prieksmeti = prieksmeti_no_db)
-    
 
-@app.route("/pievienot", methods=["POST","GET"])
+        dati = f"Pievienots skolēns - {vards} {uzvards}, skolotājs - {skolotajs_v} {skolotajs_uzv}, mācību prieksmets - {prieksmets}"
+
+        return render_template("index.html", aizsutitais = dati, skoleni = skoleni_no_db, skolotaji = skolotaji_no_db, prieksmeti = prieksmeti_no_db)
+    
+    # Get metode
+    return render_template("index.html", skoleni = skoleni_no_db,  skolotaji = skolotaji_no_db, prieksmeti = prieksmeti_no_db)
+
+@app.route("/pievienot", methods=["POST", "GET"])
 def pievienot():
     skolotaji = iegut_skolotajus()
+    skoleni = iegut_skolenus()
+    prieksmeti = iegut_prieksmetus()
+    atzimju_dati = iegut_atzimes()
+    sk_prieksmeti = iegut_sk_prieksmetus
     if request.method == "POST":
         print(request.form['skolotajs'])
-    return render_template("pievienot.html", skolotaji = skolotaji)
+    return render_template("pievienot.html", skolotaji = skolotaji, skoleni=skoleni, prieksmeti=prieksmeti, atzimes=atzimju_dati)
 
-@app.route("/atzimes")
+@app.route("/pievienot/atzimes", methods = ["POST"])
 def atzimes():
-    return render_template("atzimes.html")
+    atzime = request.form['atzime']
+    skolens = request.form['skolens']
+    prieksmets = request.form['prieksmets']
+    pievienot_atzimi(atzime,skolens, prieksmets)
+    return redirect("/pievienot")
 
 
 
